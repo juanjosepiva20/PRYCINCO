8883653ba8d509bce306e2ac592c6fc306867783
juanjosepiva20 <73726657+juanjosepiva20@users.noreply.github.com>
2020-12-08 21:04:47 -0500

    Add files via upload

---
+++ PRYCINCO/condicionales.js
@@ -0,0 +1,13 @@
+console.log("Hola desde el JavaScript");
+
+function nombreFuncion() {
+  
+    document.getElementById("msg1").innerHTML = "Hola desde un H1";
+    document.getElementById("msg2").innerHTML = "Hola desde un Label";
+    document.getElementById("msg3").innerHTML = "Hola desde un Párrafo";
+}
+
+function alertaMensaje() {
+    
+        document.getElementById("userName").innerHTML ="Se ha registrado ";
+}
